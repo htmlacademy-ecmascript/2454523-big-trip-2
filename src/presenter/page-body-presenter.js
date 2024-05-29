edfd74903b8f535b2fc03e7abdd7a74f8588e385
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2024-05-29 13:48:41 +0300

    отрисовала форму создания + точки маршрута

--- src/presenter/page-body-presenter.js
+++ src/presenter/page-body-presenter.js
@@ -1,9 +1,19 @@
 import SortView from '../view/sort-view.js';
 import TripEventsView from '../view/trip-event-view.js';
+import CreatePointView from '../view/create-point-view.js';
+import TripEventListView from '../view/trip-event-list.js';
+import PointListView from '../view/point-list-view.js';
 import { render } from '../render.js';
 
 export default class TripEventsPresenter {
-  tripEventsComponent = new TripEventsView();
+  tripEventsComponent = new TripEventsView(); // это секция
+  tripEventListComponent = new TripEventListView(); // это список - лежит в секции
+
+  //секция tripEventsComponent лежит в tripEventsContainer
+  // сортирока  SortView лежит в секции tripEventsComponent
+  // список tripEventListComponent  лежит в секции tripEventsComponent
+  // форма создания CreatePointView  лежит в списке tripEventListComponent
+  // точка маршрута лежит PointListView в списке tripEventListComponent
 
   constructor ({tripEventsContainer}) {
     this.tripEventsContainer = tripEventsContainer;
@@ -12,6 +22,13 @@ export default class TripEventsPresenter {
   init () {
     render(this.tripEventsComponent, this.tripEventsContainer);
     render(new SortView(), this.tripEventsComponent.getElement());
+    render(this.tripEventListComponent,this.tripEventsComponent.getElement());
+    render (new CreatePointView(), this.tripEventListComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render (new PointListView(), this.tripEventListComponent.getElement());
+    }
+
   }
 }
 
