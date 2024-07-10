842da182983af1f9b0ce5fe62ab0b2c8ba76d297
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2024-07-10 12:56:34 +0300

    Рефакторинг: Переименование переменных по рекомендациям наставника

--- src/const.js
+++ src/const.js
@@ -7,12 +7,12 @@ const TIME_FORMAT = 'HH:mm';
 const DATETIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 const DATETIME_FORMAT_FOR_EDIT_FORM = 'DD/MM/YY HH:mm';
 
-const TextNoEvent = {
+const NoEventText = {
   EVERYTHING:'Click New Event to create your first point',
   PAST:'There are no past events now',
   PRESENT:'There are no present events now',
   FUTURE:'There are no future events now',
 };
 
-export {POINT_TYPES, DESTINATIONS, DATE_FORMAT, TIME_FORMAT, DATETIME_FORMAT, DATETIME_FORMAT_FOR_EDIT_FORM, TextNoEvent };
+export {POINT_TYPES, DESTINATIONS, DATE_FORMAT, TIME_FORMAT, DATETIME_FORMAT, DATETIME_FORMAT_FOR_EDIT_FORM, NoEventText };
 
--- src/view/no-point-view.js
+++ src/view/no-point-view.js
@@ -1,14 +1,14 @@
 import AbstractView from '../framework/view/abstract-view.js';
-import { TextNoEvent } from '../const.js';
+import { NoEventText } from '../const.js';
 
-function createNoPoinTemplate () {
+function createNoPointTemplate () {
   return (
-    `<p class="trip-events__msg">${TextNoEvent.EVERYTHING}</p>`
+    `<p class="trip-events__msg">${NoEventText.EVERYTHING}</p>`
   );
 }
 
 export default class NoPointView extends AbstractView {
   get template () {
-    return createNoPoinTemplate();
+    return createNoPointTemplate();
   }
 }
